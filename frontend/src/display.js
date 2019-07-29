6e5dd0c2a3e73cb7be1014036c6e13ff796aab38
Leonie Dong <leoniedong@hotmail.com>
2019-07-29 16:52:57 -0400

    refactored conditional event signup button

--- frontend/src/display.js
+++ frontend/src/display.js
@@ -1,52 +1,79 @@
+function showEvent(event){
+    const eventDiv = document.querySelector('div#events')
+    eventDiv.innerHTML += `
+    <div class="event-box">
+        <div class="event-title-container" id="etc-${event.id}">
+            <h3>${event.title}</h3>
+        </div>
+        
+        <div class="event-info">
+            <p>Location: ${event.location}</p>
+            <p>Start date: ${displayDate(event.start)}</p>
+            <p>End date: ${displayDate(event.end)}</p>
+            <p>Dress code: ${event.dress_code}</p>
+            <p>Speakers: ${event.speakers}</p>
+            <p>Contact: ${event.contact_email}</p>
+            <p>Category: ${event.category}</p>
+            <p>Tags: ${event.tags}</p>
+        </div>
+    </div>`
+}
+
 function displayAllEvents(studentId) {
     fetch(EVENTS_URL)
     .then(res => res.json())
     .then(events => {
         events.forEach(event => {
+            showEvent(event)
             /** show + if not signed up */
             if (!event.students.map(student => student.id).includes(studentId)){
-                const eventDiv = document.querySelector('div#events')
-                eventDiv.innerHTML += `
-                <div class="event-box">
-                    <div class="event-title-container">
-                        <h3 style="display:inline-block;">${event.title}</h3>
-                        <i class="fa fa-calendar-plus-o sign-up-event" data-id=${event.id} aria-hidden="true" display="color:#DDE9F4"></i>
-                    </div>
+                const etc = document.getElementById(`etc-${event.id}`)
+                etc.innerHTML += `<i class="fa fa-calendar-plus-o sign-up-event" data-id=${event.id} aria-hidden="true"></i>`
+                // const eventDiv = document.querySelector('div#')
+                // eventDiv.innerHTML += `
+                // <div class="event-box">
+                //     <div class="event-title-container">
+                //         <h3>${event.title}</h3>
+                //     </div>
                     
-                    <div class="event-info">
-                        <p>Location: ${event.location}</p>
-                        <p>Start date: ${displayDate(event.start)}</p>
-                        <p>End date: ${displayDate(event.end)}</p>
-                        <p>Dress code: ${event.dress_code}</p>
-                        <p>Speakers: ${event.speakers}</p>
-                        <p>Contact: ${event.contact_email}</p>
-                        <p>Category: ${event.category}</p>
-                        <p>Tags: ${event.tags}</p>
-                    </div>
-                </div>`
+                //     <div class="event-info">
+                //         <p>Location: ${event.location}</p>
+                //         <p>Start date: ${displayDate(event.start)}</p>
+                //         <p>End date: ${displayDate(event.end)}</p>
+                //         <p>Dress code: ${event.dress_code}</p>
+                //         <p>Speakers: ${event.speakers}</p>
+                //         <p>Contact: ${event.contact_email}</p>
+                //         <p>Category: ${event.category}</p>
+                //         <p>Tags: ${event.tags}</p>
+                //     </div>
+                // </div>`
             } 
-            /** no + if signed up */
-            else {
-                const eventDiv = document.querySelector('div#events')
-                eventDiv.innerHTML += `
-                <div class="event-box">
-                    <h3>${event.title}</h3>
-                    <div class="event-info">
-                        <p>Location: ${event.location}</p>
-                        <p>Start date: ${displayDate(event.start)}</p>
-                        <p>End date: ${displayDate(event.end)}</p>
-                        <p>Dress code: ${event.dress_code}</p>
-                        <p>Speakers: ${event.speakers}</p>
-                        <p>Contact: ${event.contact_email}</p>
-                        <p>Category: ${event.category}</p>
-                        <p>Tags: ${event.tags}</p>
-                    </div>
-                </div>`
-            }
+            // /** no + if signed up */
+            // else {
+            //     const eventDiv = document.querySelector('div#events')
+            //     eventDiv.innerHTML += `
+            //     <div class="event-box">
+            //         <h3>${event.title}</h3>
+            //         <div class="event-info">
+            //             <p>Location: ${event.location}</p>
+            //             <p>Start date: ${displayDate(event.start)}</p>
+            //             <p>End date: ${displayDate(event.end)}</p>
+            //             <p>Dress code: ${event.dress_code}</p>
+            //             <p>Speakers: ${event.speakers}</p>
+            //             <p>Contact: ${event.contact_email}</p>
+            //             <p>Category: ${event.category}</p>
+            //             <p>Tags: ${event.tags}</p>
+            //         </div>
+            //     </div>`
+            // }
         })
     })
 }
 
+/** displaying events for students */
+
+
+/** displaying events for organisations */
 function displayEvent(event){
     const eventList = document.getElementById('event-list')
     eventList.innerHTML += `
@@ -70,22 +97,5 @@ function displayEvent(event){
         </div>`
 }
 
-function showEvent(event){
-    const eventDiv = document.querySelector('div#events')
-    eventDiv.innerHTML += `
-    <div class="event-box">
-        <h3>${event.title}</h3>
-        <button class="sign-up-event" data-id=${event.id}>Sign up</button>
-        <div class="event-info">
-            <p>Location: ${event.location}</p>
-            <p>Start date: ${displayDate(event.start)}</p>
-            <p>End date: ${displayDate(event.end)}</p>
-            <p>Dress code: ${event.dress_code}</p>
-            <p>Speakers: ${event.speakers}</p>
-            <p>Contact: ${event.contact_email}</p>
-            <p>Category: ${event.category}</p>
-            <p>Tags: ${event.tags}</p>
-        </div>
-    </div>`
-}
+
 
